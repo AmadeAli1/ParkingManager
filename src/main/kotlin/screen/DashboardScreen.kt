10b0c09b1b99bbdc4c4d1f006bf303ede4a52b3c
AmadeAli1 <aliamade29@gmail.com>
2023-11-13 09:49:21 +0200

    DashboardScreen.kt

--- src/main/kotlin/screen/DashboardScreen.kt
+++ src/main/kotlin/screen/DashboardScreen.kt
@@ -4,37 +4,45 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.TooltipArea
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AttachMoney
+import androidx.compose.material.icons.filled.ElectricCar
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import viewmodel.DashboardViewModel
 import java.text.NumberFormat
 import java.util.*
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
 fun DashboardScreen() {
     val viewModel = remember { DashboardViewModel() }
     val payments by viewModel.paymentChart.collectAsState()
+    val cars by viewModel.cars.collectAsState()
+    val discounts by viewModel.discounts.collectAsState()
 
     Scaffold {
-        Column(
+        FlowRow(
             modifier = Modifier.fillMaxWidth()
                 .padding(it)
-                .padding(16.dp)
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(24.dp)
         ) {
+            Surface {
+                BarChart(payments)
+            }
 
-            //PieChart(data = payments)
+            CarChart(cars)
 
-            BarChart(payments)
+            DiscountChart(discounts)
 
         }
     }
@@ -44,29 +52,39 @@ fun DashboardScreen() {
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun BarChart(map: Map<String, Double>) {
+fun BarChart(map: Map<String, Double>) = Surface(tonalElevation = 2.dp, shape = MaterialTheme.shapes.small) {
+    var width by remember { mutableStateOf(0.dp) }
+
     val colors = remember {
         listOf(
             Purple200,
             Purple500,
             Teal200,
             Purple700,
-            Blue, Color.Yellow, Color.Blue, Color.Red, Color.Green, Color.Magenta, Color.DarkGray, Color.Cyan
+            Blue,
+            Color.Yellow,
+            Color.Blue,
+            Color.Red,
+            Color.Green,
+            Color.Magenta,
+            Color.DarkGray,
+            Color.Cyan
         )
     }
-    Row(
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        modifier = Modifier.fillMaxWidth().height(330.dp),
-    ) {
 
-        map.asSequence().forEachIndexed { index, entry ->
-            Box(
-                modifier = Modifier.height(330.dp),
-                contentAlignment = Alignment.BottomCenter
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.spacedBy(2.dp)
+    Column(modifier = Modifier.padding(8.dp)) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier
+                .height(330.dp)
+                .onSizeChanged {
+                    width = it.width.dp
+                },
+        ) {
+            map.asSequence().forEachIndexed { index, entry ->
+                Box(
+                    modifier = Modifier.height(330.dp),
+                    contentAlignment = Alignment.BottomCenter
                 ) {
                     TooltipArea(tooltip = {
                         Card {
@@ -77,20 +95,180 @@ fun BarChart(map: Map<String, Double>) {
                             )
                         }
                     }) {
-                        Box(
-                            Modifier.heightIn(min = 0.dp, max = 300.dp)
-                                .height((entry.value).dp)
-                                .width(20.dp)
-                                .clip(MaterialTheme.shapes.large).background(colors[index])
-                        )
+                        Column(
+                            verticalArrangement = Arrangement.spacedBy(2.dp)
+                        ) {
+                            Box(
+                                Modifier.heightIn(min = 0.dp, max = 300.dp)
+                                    .height((entry.value).dp)
+                                    .width(20.dp)
+                                    .clip(MaterialTheme.shapes.large).background(colors[index])
+                            )
+                            Text(
+                                entry.key,
+                                style = MaterialTheme.typography.labelSmall,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
                     }
-                    Text(
-                        entry.key,
-                        style = MaterialTheme.typography.labelSmall,
-                        fontWeight = FontWeight.Bold
-                    )
                 }
             }
+        }
+
+        OutlinedButton(
+            onClick = {},
+            enabled = true,
+            modifier = Modifier.width(width),
+            shape = MaterialTheme.shapes.small
+        ) {
+            Icon(Icons.Default.AttachMoney, contentDescription = null)
+            Text("Valor total ${map.values.sum().toCurrency()}")
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun CarChart(map: Map<String, Int>) = Surface(tonalElevation = 2.dp, shape = MaterialTheme.shapes.small) {
+    var width by remember { mutableStateOf(0.dp) }
+    val colors = remember {
+        listOf(
+            Purple200,
+            Purple500,
+            Teal200,
+            Purple700,
+            Blue,
+            Color.Yellow,
+            Color.Blue,
+            Color.Red,
+            Color.Green,
+            Color.Magenta,
+            Color.DarkGray,
+            Color.Cyan
+        ).shuffled()
+    }
+    Column(modifier = Modifier.padding(8.dp)) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.height(330.dp).onSizeChanged {
+                width = it.width.dp
+            }
+        ) {
+            map.asSequence().forEachIndexed { index, entry ->
+                Box(
+                    modifier = Modifier.height(330.dp),
+                    contentAlignment = Alignment.BottomCenter
+                ) {
+                    TooltipArea(tooltip = {
+                        Card {
+                            Text(
+                                text = entry.value.toString(),
+                                style = MaterialTheme.typography.labelSmall,
+                                fontWeight = FontWeight.SemiBold, modifier = Modifier.padding(4.dp)
+                            )
+                        }
+                    }) {
+                        Column(
+                            verticalArrangement = Arrangement.spacedBy(2.dp)
+                        ) {
+                            Box(
+                                Modifier.heightIn(min = 0.dp, max = 300.dp)
+                                    .height((entry.value).dp)
+                                    .width(20.dp)
+                                    .clip(MaterialTheme.shapes.large).background(colors[index])
+                            )
+                            Text(
+                                entry.key,
+                                style = MaterialTheme.typography.labelSmall,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                    }
+                }
+            }
+        }
+        OutlinedButton(
+            onClick = {}, enabled = true,
+            modifier = Modifier.width(width),
+            shape = MaterialTheme.shapes.small
+        ) {
+            Icon(Icons.Default.ElectricCar, contentDescription = null)
+            Text("Total de carros ${map.values.sum()}")
+        }
+    }
+}
+
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun DiscountChart(map: Map<String, Double>) = Surface(tonalElevation = 2.dp, shape = MaterialTheme.shapes.small) {
+    var width by remember { mutableStateOf(0.dp) }
+
+    val colors = remember {
+        listOf(
+            Purple200,
+            Purple500,
+            Teal200,
+            Purple700,
+            Blue,
+            Color.Yellow,
+            Color.Blue,
+            Color.Red,
+            Color.Green,
+            Color.Magenta,
+            Color.DarkGray,
+            Color.Cyan
+        ).shuffled()
+    }
+    Column(modifier = Modifier.padding(8.dp)) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier
+                .height(330.dp)
+                .onSizeChanged {
+                    width = it.width.dp
+                },
+        ) {
+            map.asSequence().forEachIndexed { index, entry ->
+                Box(
+                    modifier = Modifier.height(330.dp),
+                    contentAlignment = Alignment.BottomCenter
+                ) {
+                    TooltipArea(tooltip = {
+                        Card {
+                            Text(
+                                text = entry.value.toCurrency(),
+                                style = MaterialTheme.typography.labelSmall,
+                                fontWeight = FontWeight.SemiBold, modifier = Modifier.padding(4.dp)
+                            )
+                        }
+                    }) {
+                        Column(
+                            verticalArrangement = Arrangement.spacedBy(2.dp)
+                        ) {
+                            Box(
+                                Modifier.heightIn(min = 0.dp, max = 300.dp)
+                                    .height((entry.value).dp)
+                                    .width(20.dp)
+                                    .clip(MaterialTheme.shapes.large).background(colors[index])
+                            )
+                            Text(
+                                entry.key,
+                                style = MaterialTheme.typography.labelSmall,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                    }
+                }
+            }
+        }
+        OutlinedButton(
+            onClick = {},
+            enabled = true,
+            modifier = Modifier.width(width),
+            shape = MaterialTheme.shapes.small
+        ) {
+            Text("Total perdido em descontos ${map.values.sum().toCurrency()}")
         }
     }
 }
--- src/main/kotlin/viewmodel/DashboardViewModel.kt
+++ src/main/kotlin/viewmodel/DashboardViewModel.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.stateIn
 import repository.RetrofitInstance
+import repository.toMessage
 import retrofit2.create
 import service.ParkingService
 import java.io.IOException
@@ -13,8 +14,15 @@ class DashboardViewModel {
     private val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.Unconfined + SupervisorJob())
     private val service: ParkingService = RetrofitInstance.retrofit.create()
 
-    private val _paymentChart = MutableStateFlow(emptyMap<String,Double>())
+    private val _paymentChart = MutableStateFlow(emptyMap<String, Double>())
     val paymentChart = _paymentChart.stateIn(coroutineScope, SharingStarted.WhileSubscribed(5_000), emptyMap())
+
+    private val _cars = MutableStateFlow(emptyMap<String, Int>())
+    val cars = _cars.stateIn(coroutineScope, SharingStarted.WhileSubscribed(5_000), emptyMap())
+
+    private val _discount = MutableStateFlow(emptyMap<String, Double>())
+    val discounts = _discount.stateIn(coroutineScope, SharingStarted.WhileSubscribed(5_000), emptyMap())
+
 
     init {
         findAll()
@@ -27,14 +35,35 @@ class DashboardViewModel {
                 delay(500)
                 if (response.isSuccessful) {
                     val payments = response.body()!!
-                    val groupBy = payments.groupBy {
-                        it.entranceTime.split(" ")[2]
-                    }.map { map ->
-                        Pair(map.key,map.value.sumOf { it.amount })
+                    launch(Dispatchers.Default) {
+                        val groupBy = payments.groupBy {
+                            it.entranceTime.split(" ")[2]
+                        }.map { map ->
+                            Pair(map.key, map.value.sumOf { it.amount })
+                        }
+                        _paymentChart.emit(groupBy.toMap())
                     }
-                    _paymentChart.emit(groupBy.toMap())
+
+                    launch(Dispatchers.Default) {
+                        val groupBy = payments.groupBy {
+                            it.entranceTime.split(" ")[2]
+                        }.map { map ->
+                            Pair(map.key, map.value.size)
+                        }
+                        _cars.emit(groupBy.toMap())
+                    }
+
+                    launch {
+                        val groupBy = payments.groupBy {
+                            it.entranceTime.split(" ")[2]
+                        }.map { map ->
+                            Pair(map.key, map.value.sumOf { it.discount })
+                        }
+                        _discount.emit(groupBy.toMap())
+                    }
+
                 } else {
-                    //response.errorBody()?.toMessage()?.let { onFailure(it) }
+                    response.errorBody()?.toMessage()?.let { println(it) }
                 }
             } catch (io: IOException) {
                 //onFailure("Check your internet connection")
